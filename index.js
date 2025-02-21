97d43d25d1c90ceee45d61a31964f2129ce2ad22
RanjanKumar666 <typhonazazel@gmail.com>
2025-02-21 12:07:25 +0530

    updated relay 7

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { yamux } from '@chainsafe/libp2p-yamux';
 import { createLibp2p } from 'libp2p';
 import { circuitRelayServer } from '@libp2p/circuit-relay-v2';
 import { webRTC } from '@libp2p/webrtc';
+import { identify } from '@libp2p/identify';
 
 const port = process.env.PORT || 8080;
 const railwayDomain = 'relayserver-production-adeb.up.railway.app'; // Replace with actual Railway domain
@@ -12,15 +13,14 @@ const relayServer = await createLibp2p({
 	addresses: {
 		listen: [
 			`/ip4/0.0.0.0/tcp/${port}/ws`, // Allows connections from anywhere
-			`/dns4/${railwayDomain}/tcp/${port}/ws`, // Allows clients to use Railway domain,
-			`/ip4/0.0.0.0/udp/${port + 1}/webrtc-direct`,
-			`/ip4/${railwayDomain}/udp/${port + 1}/webrtc-direct`,
+			`/dns4/${railwayDomain}/tcp/${port}/ws`, // Allows clients to use
 		],
 	},
 	transports: [webSockets(), webRTC()],
 	connectionEncrypters: [noise()],
 	streamMuxers: [yamux()],
 	services: {
+		identify: identify(),
 		relay: circuitRelayServer({
 			advertise: true,
 			reservations: true,
