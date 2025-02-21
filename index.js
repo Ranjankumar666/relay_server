a24cac765823a61f021f2f0a4b21cc4c31519478
RanjanKumar666 <typhonazazel@gmail.com>
2025-02-21 11:18:55 +0530

    updated relay 4

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const relayServer = await createLibp2p({
 	connectionEncrypters: [noise()],
 	streamMuxers: [yamux()],
 	services: {
-		circuitRelay: circuitRelayServer({
+		relay: circuitRelayServer({
 			advertise: true,
 			reservations: true,
 		}),
