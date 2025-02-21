fa8a295262bf8d19528329ef49a5ba701f39920e
RanjanKumar666 <typhonazazel@gmail.com>
2025-02-21 10:52:00 +0530

    updated address 2

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ import { webSockets } from '@libp2p/websockets';
 import { yamux } from '@chainsafe/libp2p-yamux';
 import { createLibp2p } from 'libp2p';
 import { circuitRelayServer } from '@libp2p/circuit-relay-v2';
-const port = process.env.PORT || 4002;
+
+const port = process.env.PORT || 8080;
+const railwayDomain = 'relayserver-production-adeb.up.railway.app'; // Replace with actual Railway domain
 
 const relayServer = await createLibp2p({
 	addresses: {
-		listen: [`/ip4/0.0.0.0/tcp/${port}/ws`],
+		listen: [
+			`/ip4/0.0.0.0/tcp/${port}/ws`, // Allows connections from anywhere
+			`/dns4/${railwayDomain}/tcp/${port}/ws`, // Allows clients to use Railway domain
+		],
 	},
 	transports: [webSockets()],
 	connectionEncrypters: [noise()],
@@ -19,9 +24,9 @@ const relayServer = await createLibp2p({
 
 (async () => {
 	await relayServer.start();
-	console.log('Relay Node ID -->', relayServer.peerId.toString());
+	console.log('🚀 Relay Node ID:', relayServer.peerId.toString());
 	console.log(
-		'Multiaddrs -->',
+		'🌍 Multiaddrs:',
 		relayServer.getMultiaddrs().map((s) => s.toString())
 	);
 })();
